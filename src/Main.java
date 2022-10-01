befbf5e06f3181d966591141646c6c7dbc7b1636
Dara Benyacar <dbenyacar1@gmail.com>
2022-10-01 11:02:29 -0400

    Finished tip calculator project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 //Imports scanner capability
 import java.util.Scanner;
 
+//Imports rounding to two decimal places capability
+import java.text.DecimalFormat;
+
 public class Main{
     public static void main(String[] args){
         //Allows for scanner capability
@@ -20,11 +23,20 @@ public class Main{
         double tipPerPerson = totalTipAmount/numPeople;
         double totalPerPerson = totalBillWithTip/numPeople;
 
+        //Creating a decimal format to round numbers to two decimals
+        DecimalFormat roundNumber = new DecimalFormat("0.00");
+
+        //Rounding numbers
+       String stringTotalTipAmount = roundNumber.format(totalTipAmount);
+       String stringTotalBillWithTip = roundNumber.format(totalBillWithTip);
+       String stringTipPerPerson = roundNumber.format(tipPerPerson);
+       String stringTotalPerPerson = roundNumber.format(totalPerPerson);
+
         //Printing out the information to the user
-        System.out.println("The total tip amount is $" + totalTipAmount);
-        System.out.println("The total bill including tip is $" + totalBillWithTip);
-        System.out.println("The tip per person is $" + tipPerPerson);
-        System.out.println("The total per person (bill and tip together) is $" + totalPerPerson);
+        System.out.println("The total tip amount is $" + stringTotalTipAmount);
+        System.out.println("The total bill including tip is $" + stringTotalBillWithTip);
+        System.out.println("The tip per person is $" + stringTipPerPerson);
+        System.out.println("The total per person is $" + stringTotalPerPerson);
 
     }
 }
